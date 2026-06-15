0f7070ee6736cf45680b10dec55cb7ff96e56b0a
Guiltyyy-cmd <hypoflixdormnet@gmail.com>
2026-06-15 23:21:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,68 @@
+import logging
+from logging.handlers import RotatingFileHandler
 
+# Bot Configuration
+LOG_FILE_NAME = "bot.log"
+PORT = '5010'
+OWNER_ID = 7537243058
+
+MSG_EFFECT = 5046509860389126442
+
+SHORT_URL = "" # shortner url 
+SHORT_API = "" 
+SHORT_TUT = ""
+
+# Bot Configuration
+SESSION = "LustyDormNeT"
+TOKEN = ""
+API_ID = ""
+API_HASH = ""
+WORKERS = 5
+
+DB_URI = ""
+DB_NAME = "Yae_Probot"
+
+FSUBS = [[-1003819872917, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
+# Database Channel (Primary)
+DB_CHANNEL =  -1003982607035  # just put channel id dont add ""
+# Multiple Database Channels (can be set via bot settings)
+# DB_CHANNELS = {
+#     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
+#     "-1001234567890": {"name": "Secondary DB", "is_primary": False, "is_active": True}
+# }
+# Auto Delete Timer (seconds)
+AUTO_DEL = 300
+# Admin IDs
+ADMINS = [6497757690, 6103092779]
+# Bot Settings
+DISABLE_BTN = True
+PROTECT = False
+
+# Messages Configuration
+MESSAGES = {
+    "START": "<b>›› ʜᴇʏ!! {mention}× sᴇɴᴘᴀɪ 🎊\n</b><blockquote><b>ʟᴏᴠᴇ ᴄᴏʀɴ & ᴊᴀᴠ? ɪ ᴀᴍ ᴍᴀᴅᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ғɪɴᴅ ᴡʜᴀᴛ ʏᴏᴜ'ʀᴇ ʟᴏᴏᴋɪɴɢ ꜰᴏʀ..</b></blockquote>\n<blockquote>››ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/BLURPLEOG'>彡 ᗷᒪᑌᖇᑭᒪᗴᗝᘜ 彡</a></blockquote>",
+    "FSUB": "<blockquote>›› ʜᴇʏ {mention}× sᴇɴᴘᴀɪ 🎊</blockquote>\n<blockquote><b>ʏᴏᴜʀ ғɪʟᴇ ɪs ʀᴇᴀᴅʏ ‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ sᴜʙsᴄʀɪʙᴇᴅ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ, sᴜʙsᴄʀɪʙᴇ ɴᴏᴡ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ғɪʟᴇs</b></blockquote>",
+    "ABOUT": "<b>›› ғᴏʀ ᴍᴏʀᴇ: <a href='https://t.me/HypoFlix_Network'>Cʟɪᴄᴋ ʜᴇʀᴇ</a>\n<blockquote expandable>›› ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/HypoFlix_Network'>ʜʏᴘᴏғʟɪx_ᴏғғɪᴄɪᴀʟ</a> \n›› ᴏᴡɴᴇʀ: @BlurpleOG\n›› ʟᴀɴɢᴜᴀɢᴇ: <a href='https://docs.python.org/3/'>Pʏᴛʜᴏɴ 3</a> \n›› ʟɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ ᴠ2</a> \n›› ᴅᴀᴛᴀʙᴀsᴇ: <a href='https://www.mongodb.com/docs/'>Mᴏɴɢᴏ ᴅʙ</a> \n›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @BlurpleOg</b></blockquote>",
+    "REPLY": "<b>For More Join - @LustyDormNeT</b>",
+    "SHORT_MSG": "<b>📊 ʜᴇʏ {first}, \n\n‼️ ɢᴇᴛ ᴀʟʟ ꜰɪʟᴇꜱ ɪɴ ᴀ ꜱɪɴɢʟᴇ ʟɪɴᴋ ‼️\n\n ⌯ ʏᴏᴜʀ ʟɪɴᴋ ɪꜱ ʀᴇᴀᴅʏ, ᴋɪɴᴅʟʏ ᴄʟɪᴄᴋ ᴏɴ ᴏᴘᴇɴ ʟɪɴᴋ ʙᴜᴛᴛᴏɴ..</b>",
+    "START_PHOTO": "https://graph.org/file/510affa3d4b6c911c12e3.jpg",
+    "FSUB_PHOTO": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
+    "SHORT_PIC": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
+    "SHORT": "https://telegra.ph/file/8aaf4df8c138c6685dcee-05d3b183d4978ec347.jpg"
+}
+
+def LOGGER(name: str, client_name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    formatter = logging.Formatter(
+        f"[%(asctime)s - %(levelname)s] - {client_name} - %(name)s - %(message)s",
+        datefmt='%d-%b-%y %H:%M:%S'
+    )
+    file_handler = RotatingFileHandler(LOG_FILE_NAME, maxBytes=50_000_000, backupCount=10)
+    file_handler.setFormatter(formatter)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    return logger
